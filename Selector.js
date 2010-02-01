754ffdee25248238c7ae3e32a99e1a33cdc51b54
Sean O' Shea <oshea.ie@gmail.com>
2010-01-31 22:20:51 -0800

    Adding BoundingBoxController

--- Selector.js
+++ Selector.js
@@ -24,6 +24,29 @@ dojo.declare(
 			}
 		},
 		
+		selectAll: function(args) {
+			// summary: selects all nodes
+			// args: Object: contains coordinate information for the bounding box. (optional)
+			// check to see whether this function is being invoked as a result of a bounding box.
+			if(args && args.endX != null && args.endY != null) {
+				// user has drawn a bounding box ... time to see whether any dom nodes
+				// in this container satisfy the bounding box range.
+				this.selectNone();
+				this.forInItems(function(data, id) {
+					var node = dojo.byId(id);
+					if(node && this._isBoundedByBox({coords: dojo.coords(node),
+											startX: args.startX, endX: args.endX,
+											startY: args.startY, endY: args.endY})) {
+						this._selectNode(id, true);
+					}
+				}, this);
+				return this._removeAnchor();
+			} else {
+				// just invoke the regular selectAll without bounding box arguments
+				return this.inherited(arguments);
+			}
+		},
+		
 		_selectNode: function(nodeId, shiftKey) {
 			// summary: selects a node based on nodeId
 			// nodeId: String: the id of the node to select
@@ -35,6 +58,40 @@ dojo.declare(
 			}
 			this._addItemClass(dojo.byId(nodeId), "Selected");
 			this.selection[nodeId] = 1;
+		},
+
+		_isBoundedByBox: function(args) {
+			// summary: figures out whether certain coodinates bound a particular dom node.
+			// args: contains dom node coordinates and coordinates for the
+			// positioning of the bounding box
+			var isBounded = false, tlx, tly, brx, bry;
+			var nodeTlx = args.coords.x, nodeTly = args.coords.y, nodeBrx = args.coords.x + args.coords.w, nodeBry = args.coords.y + args.coords.h;
+			// tlx, tly represents the x,y coordinates for the top left of the bounding box
+			// brx, bry represents the x,y coordinates for the bottom right of the bounding box
+			// nodeTlx, nodeTly represents the x,y coordinates for the top left of the dom node
+			// nodeBrx, nodeBry represents the x,y coordinates for the bottom right of the dom node
+			if(args.startX < args.endX) {
+				tlx = args.startX;
+				tly = args.startY;
+			} else {
+				tlx = args.endX;
+				tly = args.endY;
+			}
+			if(args.startY < args.endY) {
+				brx = args.endX;
+				bry = args.endY;
+			} else {
+				brx = args.startX;
+				bry = args.startY;
+				tlx = args.endX;
+				tly = args.endY;
+			}
+			var xBounded = (nodeTlx >= tlx || nodeBrx <= brx) && (tlx <= nodeBrx && brx >= nodeTlx) || (nodeTlx <= tlx && nodeBrx >= brx);
+			var yBounded = (tly <= nodeBry && bry >= nodeTly) || (nodeBry >= bry && nodeTly <= bry);
+			if(xBounded && yBounded) {
+				isBounded = true;
+			}
+			return isBounded;
 		},
 		
 		_getNodeId: function(nodeId, offset) {
