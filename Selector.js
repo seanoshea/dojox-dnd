4ab01a200c3edd9d406f160e0e687fffe724343b
Sean O' Shea <seanoshea@manager-alex.eye.fi>
2010-08-20 17:11:50 -0700

    Sync with dojo 1.5 implementations.

--- Selector.js
+++ Selector.js
@@ -4,108 +4,150 @@ dojo.require("dojo.dnd.Selector");
 
 dojo.declare(
 	"dojox.dnd.Selector",
-	[dojo.dnd.Selector],
+	dojo.dnd.Selector,
 	{
-		// summary: a mixin for the dojo.dnd.Selector class which adds functionality
-		// to shift the currently selected index backwards and forwards. One possible
-		// use for this mixin would be to allow a user select different dnd items using
-		// the right and left keys.
+		isSelected: function(node){
+			//	summary:
+			//		checks if node is selected
+			//	node: String|DomNode:
+			//		Node to check (id or DOM Node)
+			var id = dojo.isString(node) ? node : node.id,
+				item = this.getItem(id);
+			return item && this.selected[id];	// Boolean
+		},
 		
-		shift: function(offset, shiftKey) {
-			// summary: shifts the currently selected dnd item
-			// offset: int: the amount to bump the selection by.
-			// shiftKey: Boolean: whether or not this new selection happened when the user was holding
-			// down the shift key
+		selectNode: function(node, add){
+			//	summary:
+			//		selects a node
+			//	node: String|DomNode:
+			//		Node to select (id or DOM Node)
+			//	add: Boolean?:
+			//		If true, node is added to selection, otherwise current
+			//		selection is removed, and node will be the only selection.
+			if(!add){
+				this.selectNone();
+			}
+			var id = dojo.isString(node) ? node : node.id,
+				item = this.getItem(id);
+			if(item){
+				this._removeAnchor();
+				this.anchor = dojo.byId(node);
+				this._addItemClass(this.anchor, "Anchor");
+				this.selection[id] = 1;
+				this._addItemClass(this.anchor, "Selected");
+			}
+			return this;	// self
+		},
+		
+		deselectNode: function(node){
+			//	summary:
+			//		deselects a node
+			//	node: String|DomNode:
+			//		Node to deselect (id or DOM Node)
+			var id = dojo.isString(node) ? node : node.id,
+				item = this.getItem(id);
+			if(item && this.selection[id]){
+				if(this.anchor === dojo.byId(node)){
+					this._removeAnchor();
+				}
+				delete this.selection[id];
+				this._removeItemClass(this.anchor, "Selected");
+			}
+			return this;	// self
+		},
+		
+		selectByBBox: function(left, top, right, bottom, add) {
+			//	summary:
+			//		selects nodes by bounding box
+			//	left: Number:
+			//		Left coordinate of the bounding box
+			//	top: Number:
+			//		Top coordinate of the bounding box
+			//	right: Number:
+			//		Right coordinate of the bounding box
+			//	bottom: Number:
+			//		Bottom coordinate of the bounding box
+			//	add: Boolean?:
+			//		If true, node is added to selection, otherwise current
+			//		selection is removed, and node will be the only selection.
+
+			// user has drawn a bounding box ... time to see whether any dom nodes
+			// in this container satisfy the bounding box range.
+			if(!add){
+				this.selectNone();
+			}
+			this.forInItems(function(data, id){
+				var node = dojo.byId(id);
+				if(node && this._isBoundedByBox(node, left, top, right, bottom)){
+					this.selectNode(id, true);
+				}
+			}, this);
+			return this;	// self
+		},
+		
+		_isBoundedByBox: function(node, left, top, right, bottom) {
+			//	summary:
+			//		figures out whether certain coodinates bound a particular
+			//		dom node.
+			//	node: String|DomNode:
+			//		Node to check (id or DOM Node)
+			//	left: Number:
+			//		Left coordinate of the bounding box
+			//	top: Number:
+			//		Top coordinate of the bounding box
+			//	right: Number:
+			//		Right coordinate of the bounding box
+			//	bottom: Number:
+			//		Bottom coordinate of the bounding box
+			var c = dojo.coords(node), t;
+			// normalize input
+			if(left > right){
+				t = left;
+				left = right;
+				right = t;
+			}
+			if(top > bottom){
+				t = top;
+				top = bottom;
+				bottom = t;
+			}
+			return c.x >= left && c.x + c.w <= right && c.y >= top && c.y + c.h <= bottom;	// Boolean
+		},
+		
+		shift: function(toNext, add) {
+			//	summary:
+			//		shifts the currently selected dnd item forwards and backwards.
+			//		One possible use would be to allow a user select different
+			//		dnd items using the right and left keys.
+			//	toNext: Boolean:
+			//		If true, we select the next node, otherwise the previous one.
+			//	add: Boolean?:
+			//		If true, add to selection, otherwise current selection is
+			//		removed before adding any nodes.
 			var selectedNodes = this.getSelectedNodes();
 			if(selectedNodes && selectedNodes.length) {
-				// only delegate to _selectNode if at least one node is selected. If multiple nodes are selected
-				// assume that we go with the last selected node.
-				this._selectNode(this._getNodeId(selectedNodes[selectedNodes.length - 1].id, offset), shiftKey);
+				// only delegate to selectNode if at least one node is selected.
+				// If multiple nodes are selected assume that we go with
+				// the last selected node.
+				this.selectNode(this._getNodeId(selectedNodes[selectedNodes.length - 1].id, toNext), add);
 			}
 		},
-		
-		selectAll: function(args) {
-			// summary: selects all nodes
-			// args: Object: contains coordinate information for the bounding box. (optional)
-			// check to see whether this function is being invoked as a result of a bounding box.
-			if(args && args.endX != null && args.endY != null) {
-				// user has drawn a bounding box ... time to see whether any dom nodes
-				// in this container satisfy the bounding box range.
-				this.selectNone();
-				this.forInItems(function(data, id) {
-					var node = dojo.byId(id);
-					if(node && this._isBoundedByBox({coords: dojo.coords(node),
-											startX: args.startX, endX: args.endX,
-											startY: args.startY, endY: args.endY})) {
-						this._selectNode(id, true);
-					}
-				}, this);
-				return this._removeAnchor();
-			} else {
-				// just invoke the regular selectAll without bounding box arguments
-				return this.inherited(arguments);
-			}
-		},
-		
-		_selectNode: function(nodeId, shiftKey) {
-			// summary: selects a node based on nodeId
-			// nodeId: String: the id of the node to select
-			// shiftKey: Boolean: whether or not this new selection happened when the user was holding
-			// down the shift key
-			if(!shiftKey) {
-				// only clear the selection if the user was not holding down the shift key
-				this.selectNone();
-			}
-			this._addItemClass(dojo.byId(nodeId), "Selected");
-			this.selection[nodeId] = 1;
-		},
-		
-		_isBoundedByBox: function(args) {
-			// summary: figures out whether certain coodinates bound a particular dom node.
-			// args: contains dom node coordinates and coordinates for the
-			// positioning of the bounding box
-			var isBounded = false, tlx, tly, brx, bry, xBounded, yBounded,
-			nodeTlx = args.coords.x, nodeTly = args.coords.y, nodeBrx = args.coords.x + args.coords.w, nodeBry = args.coords.y + args.coords.h;
-			// tlx, tly represents the x,y coordinates for the top left of the bounding box
-			// brx, bry represents the x,y coordinates for the bottom right of the bounding box
-			// nodeTlx, nodeTly represents the x,y coordinates for the top left of the dom node
-			// nodeBrx, nodeBry represents the x,y coordinates for the bottom right of the dom node
-			if(args.startX < args.endX) {
-				tlx = args.startX;
-				tly = args.startY;
-			} else {
-				tlx = args.endX;
-				tly = args.endY;
-			}
-			if(args.startY < args.endY) {
-				brx = args.endX;
-				bry = args.endY;
-			} else {
-				brx = args.startX;
-				bry = args.startY;
-				tlx = args.endX;
-				tly = args.endY;
-			}
-			xBounded = (nodeTlx >= tlx || nodeBrx <= brx) && (tlx <= nodeBrx && brx >= nodeTlx) || (nodeTlx <= tlx && nodeBrx >= brx);
-			yBounded = (tly <= nodeBry && bry >= nodeTly) || (nodeBry >= bry && nodeTly <= bry);
-			if(xBounded && yBounded) {
-				isBounded = true;
-			}
-			return isBounded;
-		},
-		
-		_getNodeId: function(nodeId, offset) {
-			// summary: selects a node based on nodeId
-			// nodeId: String: the id of the node to select
-			// offset: int: the number of nodes to shift the current selection by
-			var allNodes = this.getAllNodes(), newId = nodeId, node;
-			for(var i = 0, l = allNodes.length; i < l; i++) {
-				node = allNodes[i]; 
-				if(node.id == nodeId) {
-					// have a match ... make sure we're not at the start or the end of the dnd set
-					if(!((offset == -1 && i == 0) || (i == l - 1 && offset == 1))) {
+
+		_getNodeId: function(nodeId, toNext) {
+			//	summary:
+			//		finds a next/previous node in relation to nodeId
+			//	nodeId: String:
+			//		the id of the node to use as the base node
+			//	toNext: Boolean:
+			//		If true, we select the next node, otherwise the previous one.
+			var allNodes = this.getAllNodes(), newId = nodeId;
+			for(var i = 0, l = allNodes.length; i < l; ++i) {
+				if(allNodes[i].id == nodeId) {
+					// have a match ... make sure we don't go outside
+					var j = Math.min(l - 1, Math.max(0, i + (toNext ? 1 : -1)));
+					if(i != j){
 						// we should be fine to go with the id the user has requested.
-						newId = allNodes[i + offset].id;
+						newId = allNodes[j].id;
 					}
 					break;
 				}
