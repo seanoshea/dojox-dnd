88bfbfc884596f9dca9b80b26810abe0d888b424
seanoshea <oshea.ie@gmail.com>
2010-09-14 10:39:04 -0700

    Changes to _liberalBBLogic

--- tests/testAll.js
+++ tests/testAll.js
@@ -1,9 +1,9 @@
-dojo.provide("dojox.dnd.tests.testAll");
+dojo.provide('dojox.dnd.tests.testAll');
 
 try {
-	doh.registerUrl("dojox.dnd.tests.selector", dojo.moduleUrl("dojox.dnd.tests", "test_selector.html"));
-	doh.registerUrl("dojox.dnd.tests.boundingbox", dojo.moduleUrl("dojox.dnd.tests", "test_boundingBoxController.html"));
-}catch(e){
+	doh.registerUrl('dojox.dnd.tests.selector', dojo.moduleUrl('dojox.dnd.tests', 'test_selector.html'));
+	doh.registerUrl('dojox.dnd.tests.boundingbox', dojo.moduleUrl('dojox.dnd.tests', 'test_boundingBoxController.html'));
+}catch (e) {
 	doh.debug(e);
 }
 
