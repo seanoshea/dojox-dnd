de4c9878ab42803e4160663a8f4a20925aba336b
seanoshea <oshea.ie@gmail.com>
2011-06-07 13:13:08 -0700

    AMD for dojox dnd

--- tests/testAll.js
+++ tests/testAll.js
@@ -1,10 +1,10 @@
 dojo.provide('dojox.dnd.tests.testAll');
 
 try {
-	doh.registerUrl('dojox.dnd.tests.selector', dojo.moduleUrl('dojox.dnd.tests', 'test_selector.html'));
-	doh.registerUrl('dojox.dnd.tests.boundingbox', dojo.moduleUrl('dojox.dnd.tests', 'test_boundingBoxController.html'));
+    doh.registerUrl('dojox.dnd.tests.selector', dojo.moduleUrl('dojox.dnd.tests', 'test_selector.html'));
+    doh.registerUrl('dojox.dnd.tests.boundingbox', dojo.moduleUrl('dojox.dnd.tests', 'test_boundingBoxController.html'));
 }catch (e) {
-	doh.debug(e);
+    doh.debug(e);
 }
 
 
